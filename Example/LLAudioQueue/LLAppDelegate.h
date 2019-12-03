0ec1b1b663b5f9a4b8f9262025cc633abf6e6138
wilson <wilson@wilsondeMacBook-Pro.local>
2019-12-03 23:37:52 +0800

    Initial commit

---
+++ Example/LLAudioQueue/LLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  LLAppDelegate.h
+//  LLAudioQueue
+//
+//  Created by 704110362@qq.com on 12/03/2019.
+//  Copyright (c) 2019 704110362@qq.com. All rights reserved.
+//
+
+@import UIKit;
+
+@interface LLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/LLAudioQueue/LLViewController.h
@@ -0,0 +1,13 @@
+//
+//  LLViewController.h
+//  LLAudioQueue
+//
+//  Created by 704110362@qq.com on 12/03/2019.
+//  Copyright (c) 2019 704110362@qq.com. All rights reserved.
+//
+
+@import UIKit;
+
+@interface LLViewController : UIViewController
+
+@end
